ed853e6d304eb90426cde1b1eb26a0c12418f88d
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-06-25 14:14:16 -0700

    Implementation of addTwoNumbers java

--- problems/src/com/leetcode/topInterview/linkedList/AddTwoNumbers.java
+++ problems/src/com/leetcode/topInterview/linkedList/AddTwoNumbers.java
@@ -9,6 +9,25 @@ class AddTwoNumbers{
   Explanation: 342 + 465 = 807.
   */
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode curr = new ListNode(); 
+    ListNode curr = new ListNode(0);
+    ListNode head = curr;
+    int carry = 0;
+    
+    while(l1 != null || l2 != null){
+      int l1val = l1 == null ? 0 : l1.val;
+      int l2val = l2 == null ? 0 : l2.val;
+      int sum = l1val + l2val + carry;
+      carry = sum / 10;
+      curr.next = new ListNode(sum % 10 );
+      curr = curr.next;
+      if( l1.next != null)
+        l1 = l1.next;
+      if(l2.next != null)
+        l2 = l2.next;
+    }
+    if( carry != 0)
+      curr.next = new ListNode(carry);
+    
+      return head.next;
   }
 }

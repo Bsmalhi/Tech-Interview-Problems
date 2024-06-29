2ae0f77e1a3ce66335be136b7c37d14eea845837
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-06-29 16:38:05 -0700

    Create MinStack.java

---
+++ problems/src/com/leetcode/topInterview/stack/MinStack.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+  
+    public MinStack() {
+        stack = new Stack();
+        minStack = new Stack();
+    }
+    
+    public void push(int val) {
+        if(!minStack.isEmpty() && val <= minStack.peek()){
+            minStack.push(val);
+        }
+      stack.push(val);
+    }
+    
+    public void pop() {
+        int temp = stack.pop();
+      if(!minStack.isEmpty() && temp == minStack.peek())
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        if (stackMin.isEmpty())
+            return 0;
+        return minStack.peek();
+    }
+}

39f5239d7f392e35249615dd8ccf77db9144e988
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-07-02 20:30:00 -0700

    Create LinkedListCycle.java

---
+++ problems/src/com/leetcode/topInterview/linkedList/LinkedListCycle.java
@@ -0,0 +1,12 @@
+class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+}

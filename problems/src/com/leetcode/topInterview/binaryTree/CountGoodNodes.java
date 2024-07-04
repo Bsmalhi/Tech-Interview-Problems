ff18424a787e74430a2b60fdaf1fc828cbcdf9a9
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-07-04 14:01:43 -0700

    Create CountGoodNodes.java

---
+++ problems/src/com/leetcode/topInterview/binaryTree/CountGoodNodes.java
@@ -0,0 +1,19 @@
+class CountGoodNodes{
+  // criteria for goodNode is that root.val < nodeX.val
+  public int goodNodes(TreeNode root){
+    return goodNodes(root, root.val);
+  }
+  private int goodNodes(TreeNode root, int maxVal){
+    if(root == null)
+      return 0;
+    int count = 0;
+    if(root.val > maxVal){
+      count = 1;
+      maxVal = root.val;
+    }
+    count += goodNodes(root.left, maxVal);
+    count += goodNodes(root.right, maxVal);
+
+    return count;
+  }
+}

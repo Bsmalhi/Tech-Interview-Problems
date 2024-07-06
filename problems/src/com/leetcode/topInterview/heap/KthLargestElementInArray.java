1fa6cbd0b3a7583b4761d044cbdcceaa1317a926
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-07-05 17:21:51 -0700

    Create KthLargestElementInArray.java

---
+++ problems/src/com/leetcode/topInterview/heap/KthLargestElementInArray.java
@@ -0,0 +1,11 @@
+class KthLargestElementInArray {
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        for(int n: nums){
+            minHeap.offer(n);
+            if(minHeap.size() > k )
+                minHeap.poll();
+        }
+        return minHeap.poll();
+    }
+}

9057bf9db0469cd804ecfe4bb306febda39a7e68
Baljot Malhi <malhibaljotsingh@yahoo.com>
2024-07-17 19:00:45 -0700

    Create ZigZagConversion.java

---
+++ problems/src/com/leetcode/topInterview/string/ZigZagConversion.java
@@ -0,0 +1,23 @@
+class ZigZagConversion{
+  public String zigZag(String s, int numRows){
+    if(numRows == 1)
+      return s; // bcoz zig zag can't be done
+    List<StringBuilder> list = new ArrayList<>();
+    for(int i = 0; i < numRows; i++){
+      list.add(new StringBuilder());
+    }
+    int currRow = 0;
+    boolean goingDown = false;
+    for(char c: s.toCharArray()){
+      list.get(currRow).append(c);
+      if(currRow == 0 || currRow == numRows - 1)
+        goingDown = !goingDown; // reverse the direction if hits first or last row to form zig zag
+      currRow += goingDown ? 1 : -1;
+    }
+    StringBuilder res = new StringBuilder();
+    for(String sb : list){
+      res.append(sb);
+    }
+    return sb.toString();
+  }
+}
